31a3fe13cfc01825d4f2cf5d467cf94758516db5
navneet <navneetpandey@gmail.com>
2021-09-05 15:27:21 +0200

    fixing resource

--- src/main/kotlin/no/navneet/spacegame/resources/SapceGameResource.kt
+++ src/main/kotlin/no/navneet/spacegame/resources/SapceGameResource.kt
@@ -1,5 +1,8 @@
 package no.navneet.spacegame.resources
 
+import no.navneet.spacegame.elements.Alien
+import no.navneet.spacegame.elements.Space
+import no.navneet.spacegame.match.AlienFinder
 import javax.validation.Valid
 import javax.ws.rs.Consumes
 import javax.ws.rs.POST
@@ -13,12 +16,17 @@ import javax.ws.rs.core.Response
 @Path("/space")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-class SapceGameResource {
+class SapceGameResource() {
     @POST
     @Path("/alien")
     @Valid
-    fun checkAlienExistInSpace(@PathParam("shape") alienShape: String): Response {
-
-        return Response.ok().build()
+    fun checkAlienExistInSpace(
+        @PathParam("alienPixel") alienPixel: String,
+        @PathParam("spacePixel") spacePixel: String,
+    ): Response {
+        val space = Space(spacePixel.split("\n"))
+        val alien = Alien(alienPixel.split("\n"))
+        val result = AlienFinder.findAlien(space, alien)
+        return Response.ok(result).build()
     }
 }
